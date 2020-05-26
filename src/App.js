d04407770b3ac448f23f32c4ed9a26f08b705e50
Kyungjin Josiah Park <KyungjinJPark@gmail.com>
2020-05-26 13:41:14 -0400

    firebase database connections made

--- src/App.js
+++ src/App.js
@@ -7,19 +7,30 @@ import "./custom.css";
 import FormExample from "./BootstrapForm";
 import Notebook from "./Notebook";
 
+//firebase
+import fire from "./config"
+
 class App extends Component {
-  state = {
-    notes: [
-      {
-        title: "Day 1 - The Disaster",
-        contents: "Today I started. And it was a disaster"
-      },
-      {
-        title: "Afterthought",
-        contents: "Yeah... But! At least I started. I feel pretty good about that one"
-      }
-    ]
-  };
+  constructor () {
+    super();
+
+    this.state = {
+      notes: [
+        {
+          title: "Day 1 - The Disaster",
+          contents: "Today I started. And it was a disaster"
+        },
+        {
+          title: "Afterthought",
+          contents: "Yeah... But! At least I started. I feel pretty good about that one"
+        }
+      ]
+    };
+  }
+
+  componentDidMount() {
+    fire.database().ref("woot/notes").on("value", this.firebaseGotData, this.firebaseGotErr);
+  }
 
   removeNote = index => {
     const { notes } = this.state;
@@ -38,8 +49,28 @@ class App extends Component {
     });
   };
 
-  firebaseTest = () => {
-    
+  firebaseTestPush = () => {
+    fire.database().ref("woot/notes").set([{title: "Test Push", contents: "this was a test push"}]);
+  }
+  firebaseTestPull = () => {
+    fire.database().ref("woot").once("value").then((snapshot) => {
+      console.log(snapshot.val());
+    });
+  }
+  firebaseTestUpdate = () => {
+    fire.database().ref("woot/notes").update([{title: "AAAAAAA", contents: "aaaaaaaaaaaaaaaaaaaa"}]);
+  }
+  firebaseGotData = (data) => {
+    // const notesData = data.val();
+    // const arrayRef = Object.keys(notesData);
+    // const notesArr = notesData[arrayRef[0]];
+    // for (let i = 0; i < notesArr.length; i++) {
+    //   console.log(notesArr[i]);
+    // }
+  }
+  firebaseGotErr = (err) => {
+    console.log("ERRROR");
+    console.log(err);
   }
 
   render() {
@@ -48,7 +79,9 @@ class App extends Component {
     return (
       <Container className="woot">
 
-        <button onClick={this.firebaseTest}>Testme</button>
+        <button onClick={this.firebaseTestPush}>push test</button>
+        <button onClick={this.firebaseTestPull}>pull test</button>
+        <button onClick={this.firebaseTestUpdate}>update test</button>
         
         <FormExample handleSubmit={this.handleSubmit} />
         <hr />
